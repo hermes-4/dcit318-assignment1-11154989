554bdf7bb0152b2fb85bcf76140650ccf4abcab5
ebenezer <ebenezer.acquah@oyaghana.com>
2025-08-05 13:57:21 +0000

    Added trianglr type identifier project

---
+++ TriangleTypeIdentifier/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace TriangleTypeIdentifier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Triangle Type Identifier");
+
+            Console.Write("Enter side A: ");
+            string inputA = Console.ReadLine();
+            Console.Write("Enter side B: ");
+            string inputB = Console.ReadLine();
+            Console.Write("Enter side C: ");
+            string inputC = Console.ReadLine();
+
+            if (double.TryParse(inputA, out double a) &&
+                double.TryParse(inputB, out double b) &&
+                double.TryParse(inputC, out double c))
+            {
+                if (IsValidTriangle(a, b, c))
+                {
+                    string triangleType = GetTriangleType(a, b, c);
+                    Console.WriteLine($"This is a(n): {triangleType} triangle.");
+                }
+                else
+                {
+                    Console.WriteLine("These sides do not form a valid triangle.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid input: Please enter numbers only.");
+            }
+
+        }
+
+        static bool IsValidTriangle(double a, double b, double c)
+        {
+            return (a + b > c) && (a + c > b) && (b + c > a);
+        }
+
+        static string GetTriangleType(double a, double b, double c)
+        {
+            if (a == b && b == c)
+                return "Equilateral";
+            else if (a == b || b == c || a == c)
+                return "Isosceles";
+            else
+                return "Scalene";
+        }
+    }
+}
